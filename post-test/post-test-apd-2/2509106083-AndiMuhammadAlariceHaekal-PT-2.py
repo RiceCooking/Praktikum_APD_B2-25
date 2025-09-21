99994a2b3aa1e8defa81cdb9fe6380f33dfd3f32
Haekal <andihaekal338@gmail.com>
2025-09-21 10:38:31 +0800

    Kelebihan spasi di baris 2

--- post-test/post-test-apd-2/2509106083-AndiMuhammadAlariceHaekal-PT-2.py
+++ post-test/post-test-apd-2/2509106083-AndiMuhammadAlariceHaekal-PT-2.py
@@ -1,5 +1,5 @@
 nama = str(input("Masukkan Nama\t\t :"))
-nim = int(input("Masukkan NIM\t\t : "))
+nim = int(input("Masukkan NIM\t\t :"))
 dana = float(input("Masukkan Uang Belanja\t :"))
 
 print(f"{nama} dengan NIM {nim}, ingin berbelanja dengan dana Rp.{dana}")
