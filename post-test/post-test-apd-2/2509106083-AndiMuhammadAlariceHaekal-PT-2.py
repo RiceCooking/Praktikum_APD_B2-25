e175afd20ec94c11e94d4597bfdf6e558a27dcb1
Haekal <andihaekal338@gmail.com>
2025-09-21 13:34:55 +0800

    Menambahin sistem harga dan pajak

--- post-test/post-test-apd-2/2509106083-AndiMuhammadAlariceHaekal-PT-2.py
+++ post-test/post-test-apd-2/2509106083-AndiMuhammadAlariceHaekal-PT-2.py
@@ -3,3 +3,8 @@ nim = int(input("Masukkan NIM\t\t :"))
 dana = float(input("Masukkan Uang Belanja\t :"))
 
 print(f"{nama} dengan NIM {nim}, ingin berbelanja dengan dana Rp.{dana}")
+harga = 15000
+pajakmenu = [0.05, 0.08, 0.10]
+pajakPlele = harga * pajakmenu[0]
+pajakMayam = harga * pajakmenu[1]
+pajakNpadang = harga * pajakmenu[2]
