300390ba9005f5a9b9817d61b1b9193aea576ad1
Haekal <andihaekal338@gmail.com>
2025-09-26 14:08:28 +0800

    aaa

--- kelas/pertemuan3_2509106083.py
+++ kelas/pertemuan3_2509106083.py
@@ -7,9 +7,11 @@ a = [5, 6]
 print(sum(a))
 print("\n Masukkan angka")
 x = int(input())
-if(x < 0 or x > 5):
+if(x < 0 and x > 5):
     print("Error")
 if(x > 0 and x < 5):
     print("Berhasil")
+if(x == 6):
+    print("Enam")
 
 
