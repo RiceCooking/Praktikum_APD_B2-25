ff0cdb5f7eeff344a54f142876498b752f4cd8c9
Haekal <andihaekal338@gmail.com>
2025-09-26 14:04:02 +0800

    aa

--- kelas/pertemuan3_2509106083.py
+++ kelas/pertemuan3_2509106083.py
@@ -2,5 +2,14 @@
 # if angka > 5:
 #     print("Angka lebih besar dari 5")
 
+
 a = [5, 6]
 print(sum(a))
+print("\n Masukkan angka")
+x = int(input())
+if(x < 0 or x > 5):
+    print("Error")
+if(x > 0 and x < 5):
+    print("Berhasil")
+
+
