54a8134b1b7c0bec1b01fe5e09b11ecea1f3b162
Haekal <andihaekal338@gmail.com>
2025-10-03 14:31:19 +0800

    aa

--- kelas/pertemuan4_2509106083.py
+++ kelas/pertemuan4_2509106083.py
@@ -4,3 +4,15 @@ for a in range(10 , 0, -1):
 data = ((250, "A"), (251, "B"))
 for x, y in data:
     print(x ,y)
+
+for i in range(1, 4):# Mengontrol baris dalam tabel perkalian
+    for j in range(1, 5):# Mengontrol kolom dalam tabel perkalian
+        print(f'{i} x {j} = {i * j}')
+    print('') #biar ada jarak tiap iterasi
+
+jawab = 'ya'
+hitung = 0
+while(jawab == 'ya'):
+    hitung += 1
+    jawab = input("Ulang lagi? ")
+print(f"Total perulangan: {hitung}")
