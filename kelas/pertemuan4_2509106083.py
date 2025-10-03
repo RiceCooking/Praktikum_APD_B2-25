81e9e83cbba8b1181cad9564e4aec997641572c9
Haekal <andihaekal338@gmail.com>
2025-10-03 14:35:06 +0800

    gagag

--- kelas/pertemuan4_2509106083.py
+++ kelas/pertemuan4_2509106083.py
@@ -1,18 +1,27 @@
-for a in range(10 , 0, -1):
-    print(a)
+# for a in range(10 , 0, -1):
+#     print(a)
 
-data = ((250, "A"), (251, "B"))
-for x, y in data:
-    print(x ,y)
+# data = ((250, "A"), (251, "B"))
+# for x, y in data:
+#     print(x ,y)
 
-for i in range(1, 4):# Mengontrol baris dalam tabel perkalian
-    for j in range(1, 5):# Mengontrol kolom dalam tabel perkalian
-        print(f'{i} x {j} = {i * j}')
-    print('') #biar ada jarak tiap iterasi
+# for i in range(1, 4):# Mengontrol baris dalam tabel perkalian
+#     for j in range(1, 5):# Mengontrol kolom dalam tabel perkalian
+#         print(f'{i} x {j} = {i * j}')
+#     print('') #biar ada jarak tiap iterasi
 
-jawab = 'ya'
-hitung = 0
-while(jawab == 'ya'):
-    hitung += 1
-    jawab = input("Ulang lagi? ")
-print(f"Total perulangan: {hitung}")
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi? ")
+# print(f"Total perulangan: {hitung}")
+
+# angka = [2, 5, 8, 12, 15, 7, 20]
+# print("Mencari angka pertama yang lebih besar dari 10...")
+# for n in angka:
+#     print(f"Sekarang memeriksa angka: {n}")
+#     if n > 10:
+#         print(f"Angka {n} lebih besar dari 10, Perulangan berhenti.")
+#         break
+# print("Program selesai.")
