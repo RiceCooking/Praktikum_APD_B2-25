75823fbe315bc6a3d8f4673a20388d8ab5cb65a0
Haekal <andihaekal338@gmail.com>
2025-10-10 17:52:31 +0800

    80%

--- kelas/pertemuan5_250916083.py
+++ kelas/pertemuan5_250916083.py
@@ -2,8 +2,17 @@
 # print(mobil[:2])#[start : stop : step]
 # print(mobil[:1:-1])#step:-1 dari belakang. index di mobil 3..
 # del mobil[1]
-# print(mobil)# honda hilanh kareana di inddex ke 1 = "honda"
-animal = ["Kucing", "Anjing", "Kelinci", "Hamster", ["Penyu", "Kura-Kura"]]
-animal[4].insert(0, "Hiu")#di index ke 4 pada animal di tambahkan hiu ke index 0 dan index yang awalnya 0 dan 1 -> 1 dan 2 karena hiu jadi yang pertama
-print(animal)
-print(len(animal))
+# # print(mobil)# honda hilanh kareana di inddex ke 1 = "honda"
+# animal = ["Kucing", "Anjing", "Kelinci", "Hamster", ["Penyu", "Kura-Kura"]]
+# animal[4].insert(0, "Hiu")#di index ke 4 pada animal di tambahkan hiu ke index 0 dan index yang awalnya 0 dan 1 -> 1 dan 2 karena hiu jadi yang pertama
+# print(animal)
+# print(len(animal))
+# del animal[:3]
+# print(animal)
+hewan = ["Ikan", "Kelinci", "Kucing", "Sapi"]
+empat = ["Kucing", "Sapi"]
+
+for ik in hewan:
+    print(ik)
+    if ik in empat:
+        print(ik)
