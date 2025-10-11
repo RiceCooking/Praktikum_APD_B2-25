910777c288006e2ffdbf82d357d6643227907846
Haekal <andihaekal338@gmail.com>
2025-10-11 17:45:08 +0800

    sistem clear

--- post-test/post-test-apd-5/2509106083-AndiMuhammadAlariceHaekal.py
+++ post-test/post-test-apd-5/2509106083-AndiMuhammadAlariceHaekal.py
@@ -18,11 +18,13 @@ while True:
         pw = input("Password\t:")
         for usn1 in data:
             if usn1[0] == usn: # kan di cek satu satu tuh dan di index 0 dari tiap element list yang ada di data, index 0 itu username
-                print("Username tidak tersedia")
+                print("Username tidak tersedia", end = "")
+                input()
                 break
         else:
             data.append([usn, pw, "User", []])
-            print("Registrasi berhasil")
+            print("Registrasi berhasil", end="")
+            input()
     elif opsi == "2":
         print("Halaman Login")
         usn = input("Username\t:")
@@ -31,5 +33,6 @@ while True:
         print("""
 Terimakasih telah menggunakan Playlist Musik
 Anda keluar dari Aplikasigit
-              """)
+              """, end = "")
+        input()
         break
