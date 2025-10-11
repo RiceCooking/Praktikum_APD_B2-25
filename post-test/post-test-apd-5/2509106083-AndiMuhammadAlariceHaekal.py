d04752ff10df97cc9db98d78fb29de9f210c26ff
Haekal <andihaekal338@gmail.com>
2025-10-11 17:33:17 +0800

    sistem sing in tapi bagian login belum

--- post-test/post-test-apd-5/2509106083-AndiMuhammadAlariceHaekal.py
+++ post-test/post-test-apd-5/2509106083-AndiMuhammadAlariceHaekal.py
@@ -1,3 +1,33 @@
+import os
 data = [
     ["developer", "123", "Admin", []]
         ]
+
+while True:
+    os.system("cls || clear")
+    print("""
+=== Halaman Masuk Playlist Musik ==
+1. Registrasi
+2. Login
+3. Keluar
+""")
+    opsi = input("Pilih Opsi\t:")
+    if opsi == "1":
+        usn = input("Username\t:")
+        pw = input("Password\t:")
+        for usn1 in data:
+            if usn1[0] == usn: # kan di cek satu satu tuh dan di index 0 dari tiap element list yang ada di data, index 0 itu username
+                print("Username tidak tersedia")
+                break
+        else:
+            data.append([usn, pw, "User", []])
+            print("Registrasi berhasil")
+    elif opsi == "2":
+        usn = input("Username\t:")
+        pw = input("Password\t:")
+    elif opsi == "3":
+        print("""
+Terimakasih telah menggunakan Playlist Musik
+              Anda keluar dari Aplikasi
+              """)
+        break
