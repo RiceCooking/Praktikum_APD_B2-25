e165c9629714dcba81184cd0184d1625d8c80409
Haekal <andihaekal338@gmail.com>
2025-10-17 16:38:57 +0800

    aa

--- kelas/peryemuan6_2509106083.py
+++ kelas/peryemuan6_2509106083.py
@@ -30,16 +30,77 @@
 # print(f"Ini ambil intergral -> {kalkulus[f"Intergral"]}")# aksesnya harus pakai  keys
 # print(f"Ini pakai var:dict.keys() {kalkulus.keys()}")
 
-Biodata = { 
-"Nama" : "Ananda Daffa Harahap", 
-"NIM" : 2409106050, 
-"KRS" : ["Pemrograman Web", "Struktur Data", "Basis Data", "Algoritma"], 
-"Mahasiswa_Aktif" : True, 
-"Social Media" : {  "Instagram" : "daffahrhap"  }
+# Biodata = { 
+# "Nama" : "Ananda Daffa Harahap", 
+# "NIM" : 2409106050, 
+# "KRS" : ["Pemrograman Web", "Struktur Data", "Basis Data", "Algoritma"], 
+# "Mahasiswa_Aktif" : True, 
+# "Social Media" : {  "Instagram" : "daffahrhap"  }
 
-}
-print("\nIni akses Pakai Style dict[keys][values:list(index)] KRS index ke 1")
-print(Biodata["KRS"][1])
-print("\nIni akses Pakai dict.get() KRS index ke 1")
-print(Biodata.get("KRS")[1])
-print(Biodata.get("Social Media")["Instagram"])
+# }
+# print(Biodata["Nama"])
+# print(Biodata.get("Nama"))
+# print("\nIni akses Pakai Style dict[keys][values:list(index)] KRS index ke 1")
+# print(Biodata["KRS"][1])
+# print("\nIni akses Pakai dict.get() KRS index ke 1")
+# print(Biodata.get("KRS")[1])
+# print(Biodata.get("Social Media")["Instagram"])
+
+
+# mahasiswa = {
+#     83 :{
+#         "Nama" : "Andi",
+#         "Kelas" : "B",
+#         "Hobi" : "Tidur"
+#     },
+#         80 :{
+#         "Nama" : "Budi",
+#         "Kelas" : "A",
+#         "Hobi" : "Ngantuk"
+#     },
+# }
+
+# for i, j in mahasiswa.items():
+#     print(f"{i}")
+#     for x,y in j.items():
+#         print(x, y)
+#     print("")
+#Bagian Update
+# Film = { 
+# "Avenger Endgame" : "Action", 
+# "Sherlock Holmes" : "Mystery", 
+# "The Conjuring" : "Horror" 
+# } 
+ 
+# #Sebelum Diubah 
+# print(Film) 
+# Film["Gen Z"] = "Action" 
+# Film.update({"Naruto" : "Comedy"}) 
+# Film.update({"The Conjuring" : "Tragedy"}) # kalo keys nya sudah ada otomatis ganti valuenya
+# Film["Sherlock Holmes"] = "Action" 
+# print(Film) 
+
+data = { 
+"Nama" : "Daffa", 
+"Umur" : 19, 
+"Jurusan" : "Informatika" 
+} 
+#Sebelum Dihapus 
+print(data) 
+ 
+del data["Nama"] 
+#Setelah diubah 
+print(data) 
+ 
+#memanggil data yang telah dihapus 
+print(data.get("Nama")) 
+
+
+key = "apel", "jeruk", "mangga" 
+ 
+value = 1 
+ 
+buah = dict.fromkeys(key, value) 
+ #{'apel': 1, 'jeruk': 1, 'mangga': 1} 
+print(buah) 
+ 
