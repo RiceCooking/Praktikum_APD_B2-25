f03499572c8e7c10fe70fca047031357f0ab2dc8
Haekal <andihaekal338@gmail.com>
2025-10-30 16:50:33 +0800

    tes

--- kelas/pertemuan8_2509106083.py
+++ kelas/pertemuan8_2509106083.py
@@ -1,7 +1,33 @@
-tes = input("Massukan inputan\t:")
-if tes.isdigit():
-    print("Pasti angka")
-else:
-    print("Bukan angka")
+# tes = input("Massukan inputan\t:")
+# if tes.isdigit():
+#     print("Pasti angka")
+# else:
+#     print("Bukan angka")
 
-print(max([1, 3, 7]))
+# print(max([1, 3, 7]))
+
+
+# cek = "aaaaaaaaa".upper()
+# print(cek)
+
+# from math import sqrt
+# print(sqrt(16))
+# print(factorial(4))
+
+# import math as m
+# print(m.sqrt(16))
+# print(m.factorial(4))
+
+# import random
+# karakter = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&"
+# kata_sandi = ""
+# for t in range(12):
+#     kata_sandi += random.choice(karakter)
+# print(kata_sandi)
+
+
+# cek = int("1")
+# if cek == 1:
+#     print("apa")
+
+print("hello".upper())
