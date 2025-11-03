4fa84017a5311a86bb83325d6be5f7f94dd4d6e0
Haekal <andihaekal338@gmail.com>
2025-11-03 23:19:03 +0800

    Fitur fiturnya harusnya sudah

--- post-test/post-test-apd-8/display.py
+++ post-test/post-test-apd-8/display.py
@@ -5,13 +5,17 @@ def tampilkan_playlist(data, username):
         print("Belum ada playlist untuk ditampilkan.")
         return
 
-    table = PrettyTable()
-    table.field_names = ["No", "Judul Playlist", "Total Lagu"]
-
     for idx, plylist in enumerate(data[username]["playlist"], start=1):
-        table.add_row([idx, plylist[0], len(plylist[1])])
-
-    print(table)
+        print(f"\nPlaylist {idx}: {plylist[0]}")
+        
+        if not plylist[1]:
+            print("   (Belum ada musik dalam playlist ini)")
+        else:
+            table = PrettyTable()
+            table.field_names = ["No", "Judul", "Artis", "Genre", "Tahun", "Rating"]
+            for i, lagu in enumerate(plylist[1], start=1):
+                table.add_row([i, lagu[0], lagu[1], lagu[2], lagu[3], lagu[4]])
+            print(table)
 
 def tampilkan_musik(plylist):
     if len(plylist[1]) == 0:
